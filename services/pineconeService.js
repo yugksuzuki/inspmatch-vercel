f2f22ae3f009074e58a90cc56bede64cbc954115
yugksuzuki <g-suzuki@hotmail.com>
2025-05-27 14:21:09 -0300

    fix: corrigido uso da Pinecone API moderna

--- services/pineconeService.js
+++ services/pineconeService.js
@@ -1,28 +1,23 @@
-const { Pinecone } = require('@pinecone-database/pinecone');
+const { PineconeClient } = require('@pinecone-database/pinecone');
+const client = new PineconeClient();
 
-const pinecone = new Pinecone({ apiKey: process.env.PINECONE_API_KEY });
-const index = pinecone.index(process.env.PINECONE_INDEX);
-const namespace = process.env.PINECONE_NAMESPACE || 'default';
+async function initPinecone() {
+  await client.init({
+    environment: process.env.PINECONE_ENVIRONMENT,
+    apiKey: process.env.PINECONE_API_KEY,
+  });
+  return client.Index(process.env.PINECONE_INDEX);
+}
 
-async function upsertVector(id, vector, metadata = {}) {
-  await index.upsert([
-    {
-      id,
-      values: vector,
-      metadata,
-    },
-  ], namespace);
+async function upsertVector(id, vector) {
+  const index = await initPinecone();
+  await index.upsert({ upsertRequest: { vectors: [{ id, values: vector }] } });
 }
 
 async function queryVector(vector) {
-  const result = await index.query({
-    vector,
-    topK: 1,
-    includeMetadata: true,
-    namespace,
-  });
-
-  return result.matches?.[0];
+  const index = await initPinecone();
+  const result = await index.query({ queryRequest: { vector, topK: 1, includeMetadata: false } });
+  return result.matches[0];
 }
 
 module.exports = { upsertVector, queryVector };
